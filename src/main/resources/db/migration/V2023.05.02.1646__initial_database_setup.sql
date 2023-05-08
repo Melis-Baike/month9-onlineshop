5ce62e8937e2da02952c9df5cb93c9f04c08a6c9
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-08 16:31:50 +0600

    Improved first migration. Added new migration

--- src/main/resources/db/migration/V2023.05.02.1646__initial_database_setup.sql
+++ src/main/resources/db/migration/V2023.05.02.1646__initial_database_setup.sql
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS products (
     name VARCHAR(128) NOT NULL,
     image VARCHAR(256) NOT NULL,
     quantity BIGINT NOT NULL,
-    description VARCHAR(512) NOT NULL,
+    description VARCHAR(2048) NOT NULL,
     price FLOAT(8) NOT NULL,
     category_id BIGINT NOT NULL,
     brand_id BIGINT NOT NULL,
@@ -89,7 +89,7 @@ CREATE TABLE IF NOT EXISTS baskets_products (
 CREATE TABLE IF NOT EXISTS reviews (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     text VARCHAR(1024) NOT NULL,
-    rating INT NOT NULL,
+    rating FLOAT(8) NOT NULL,
     user_id BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     CONSTRAINT fk_user
@@ -115,41 +115,81 @@ INSERT INTO roles(name)
 VALUES ('ROLE_GUEST'),('ROLE_USER'),('ROLE_ADMIN');
 
 INSERT INTO products(name, image, quantity, description, price, category_id, brand_id)
-VALUES  ('Apple iPhone 12', '../static/images/Iphone12.jpg', 50,
+VALUES  ('Apple iPhone 12', '/images/Iphone12.jpg', 50,
         'The iPhone 12 features a 6.1-inch Super Retina XDR display and Ceramic Shield front cover. A14 Bionic chip ' ||
         'with next-generation Neural Engine. 5G capable. Advanced dual-camera system with 12MP Ultra Wide and ' ||
         'Wide cameras. Night mode, Deep Fusion, and Smart HDR 3. iOS with redesigned widgets on the Home screen, ' ||
         'all-new App Library, App Clips, and more.', 699, 2, 1),
-        ('Samsung Galaxy S21', '../static/images/GalaxyS21.jpg', 30,
+        ('Samsung Galaxy S21', '/images/GalaxyS21.jpg', 30,
         'The Samsung Galaxy S21 features a 6.2-inch Dynamic AMOLED 2X display and Gorilla Glass Victus front cover. ' ||
         'Exynos 2100 or Snapdragon 888 chipset with next-generation Neural Engine. 5G capable. Advanced ' ||
         'triple-camera system with 12MP Ultra Wide, Wide, and Telephoto cameras. Night mode, 8K video recording, ' ||
         'and Smart AI. Android 11 with One UI 3.1.', 799, 2, 2),
-        ('Sony PlayStation 5', '../static/images/PS5.jpg', 10,
+        ('Sony PlayStation 5', '/images/PS5.jpg', 10,
         'The Sony PlayStation 5 is a next-generation gaming console with a custom AMD Zen 2 processor and RDNA ' ||
         '2 graphics engine. 16GB GDDR6 RAM and 825GB SSD storage. 4K UHD Blu-ray drive. DualSense wireless ' ||
         'controller with haptic feedback and adaptive triggers. Supports 4K gaming and up to 120fps.', 499, 6, 3),
-        ('Apple Watch Series 6', '../static/images/AppleWatch6.jpg', 30,
-         'Smartwatch with always-on retina display, ECG and fitness tracking', 399, 8, 1),
-        ('Bose QuietComfort', '../static/images/BoseQuietComfort.jpg', 20,
-         'Over-ear noise cancelling headphones with Bluetooth and Alexa voice control', 349, 9, 4),
-        ('Dell XPS 13', '../static/images/DellXPS13.jpg', 10, '13-inch laptop with Intel Core i7 processor and 16GB RAM'
-        , 1200, 1, 5),
-        ('Apple iPad Pro', '../static/images/AppleIpadPro.jpg', 15, '12.9-inch tablet with M1 chip, 256GB storage ' ||
-         'and Liquid Retina display', 1099,
-         3, 1),
-        ('LG OLED CX', '../static/images/LGOLEDXC.jpg', 5, '65-inch OLED TV with 4K resolution and Dolby Atmos sound',
-         2499, 4, 6),
-        ('Canon EOS R5', '../static/images/CanonEOSR5.jpg', 8, 'Full-frame mirrorless camera with 45MP sensor and ' ||
-         '8K video recording', 3899, 5, 7),
-        ('HP Spectre x360', '../static/images/HPSpectrex360.jpg', 12, '2-in-1 laptop with 15.6-inch 4K touchscreen ' ||
-         'and Intel Core i7 processor', 1599,7, 8),
-        ('Sony WH-1000XM4', '../static/images/SonyWH-1000XM4.jpg', 25, 'Over-ear headphones with noise-cancellation' ||
-         'and up to 30-hour battery', 349,9, 3),
-        ('Alienware Aurora R10', '../static/images/AlienwareAuroraR10.jpg', 8, 'Gaming PC with AMD Ryzen 9 processor, ' ||
-         '16GB RAM and NVIDIA GeForce RTX 3080 graphics card', 2899, 1, 9),
-        ('Canon PIXMA TR150', '../static/images/CanonPIXMATR150.jpg', 20, 'Portable wireless printer with 1.44-inch ' ||
-         'OLED display and AirPrint support',199, 10, 7),
-        ('Sonos One', '../static/images/SonosOne.jpg', 8, 'Smart speaker with voice control and powerful sound', 199, 11, 10),
-        ('MacBook Air M1', '../static/images/MacBookAirM1.jpg', 2, 'MacBook Air M1 - a laptop from Apple with an M1 ' ||
-         'processor and a long battery life on a single charge', 1299, 7, 1);
+        ('Apple Watch Series 6', '/images/AppleWatch6.jpg', 30, 'The Apple Watch Series 6 is a feature-packed ' ||
+         'smartwatch designed for fitness enthusiasts and tech-savvy users. It boasts advanced health monitoring ' ||
+         'capabilities, such as blood oxygen and ECG monitoring, as well as a range of fitness tracking features, ' ||
+         'such as GPS and workout tracking. It is powered by an Apple S6 chip and runs on watchOS 7. It also offers ' ||
+         'seamless integration with other Apple devices, allowing users to receive notifications, control music ' ||
+         'playback, and make calls without ever taking their phone out of their pocket. The watch comes in a ' ||
+         'variety of finishes and bands, making it a stylish accessory as well as a functional device.', 399, 8, 1),
+        ('Bose QuietComfort', '/images/BoseQuietComfort.jpg', 20,
+         'Bose QuietComfort is a noise-canceling headphone developed by the Bose company. They provide comfortable ' ||
+         'listening experience by reducing the noise from the surrounding environment, allowing you to focus on the ' ||
+         'sound. They are equipped with a built-in microphone for calls and music control, as well as a battery ' ||
+         'that provides up to 20 hours of continuous playback. The headphones can be connected to a device via ' ||
+         'Bluetooth or cable. They are available in several colors and have different sizes and shapes to fit ' ||
+         'different head types.', 349, 9, 4),
+        ('Dell XPS 13', '/images/DellXPS13.jpg', 10, 'Dell XPS 13 is a laptop developed by Dell that features a ' ||
+         'compact design and high performance. It is equipped with a latest generation Intel Core i5 or i7 processor, ' ||
+         'which ensures fast and smooth task execution. This laptop also has 8 or 16 GB of RAM and a fast solid-state ' ||
+         'drive (SSD) with a capacity ranging from 256 GB to 2 TB, providing enough space for file storage and ' ||
+         'quick access to them.', 1200, 7, 5),
+        ('Apple iPad Pro', '/images/AppleIpadPro.jpg', 15, 'Apple iPad Pro is a tablet developed by Apple that ' ||
+         'boasts high performance and a plethora of features. It features a Liquid Retina display with a screen ' ||
+         'size of 11 or 12.9 inches and a resolution of up to 2732 x 2048 pixels, providing clear and vibrant image ' ||
+         'and video display.', 1099, 3, 1),
+        ('LG OLED CX', '/images/LGOLEDCX.jpg', 5, 'LG OLED CX is a television with OLED technology that provides ' ||
+         'deep blacks and bright colors. It is equipped with the α9 Gen 3 processor, supports image quality ' ||
+         'enhancement technologies and a screen dimming function. Additionally, it has Smart TV functionality.', 2499,
+         4, 6),
+        ('Canon EOS R5', '/images/CanonEOSR5.jpg', 8, 'The Canon EOS R5 is a high-end mirrorless camera with a 45MP ' ||
+         'full-frame sensor, 8K video recording, and advanced autofocus system. With its weather-sealed body and ' ||
+         'impressive performance, this camera is perfect for professional photographers and videographers.', 3899, 5, 7),
+        ('HP Spectre x360', '/images/HPSpectrex360.jpg', 12, 'HP Spectre x360 is a laptop with a display that can ' ||
+         'be rotated 360 degrees, allowing it to be used as a tablet or in tent mode. It is equipped with an Intel ' ||
+         'Core processor, has a high screen resolution and supports touch input technology. Additionally, it has a ' ||
+         'built-in fingerprint scanner, providing more secure access to the device.', 1599,7, 8),
+        ('Sony WH-1000XM4', '/images/SonyWH-1000XM4.jpg', 25, 'Sony WH-1000XM4 are wireless headphones with active ' ||
+         'noise-cancellation function. They have high sound quality, a comfortable form and lightweight design, ' ||
+         'allowing for long-term use without discomfort. The headphones feature an automatic pause when taken off ' ||
+         'the head and resume when put back on. They also support wireless Bluetooth connectivity and have a long ' ||
+         'battery life from a single charge.', 349, 9, 3),
+        ('Alienware Aurora R10', '/images/AlienwareAuroraR10.jpg', 8, 'Alienware Aurora R10 is a gaming desktop ' ||
+         'computer equipped with an AMD Ryzen processor, NVIDIA GeForce graphics card, and high-speed RAM. It has ' ||
+         'a compact chassis, but still provides powerful performance for running modern games. In addition, it ' ||
+         'supports virtual reality technology, has numerous ports for connecting peripheral devices, and is ' ||
+         'equipped with liquid cooling system for more stable operation.', 2899, 1, 9),
+        ('Canon PIXMA TR150', '/images/CanonPIXMATR150.jpg', 20, 'Canon PIXMA TR150 is a portable printer for ' ||
+         'printing documents and photos. It has a compact size and lightweight design, making it convenient for ' ||
+         'use on the go. The printer is equipped with wireless connectivity and supports printing via mobile ' ||
+         'devices and cloud services. It also has automatic duplex printing, which saves paper, and the ability ' ||
+         'to print borderless photos for a more professional look.',199, 10, 7),
+        ('Sonos One', '/images/SonosOne.jpg', 8, 'Sonos One is a wireless speaker with support for voice control ' ||
+         'through Amazon Alexa or Google Assistant. It has a compact size, but provides quality sound thanks to ' ||
+         'the unique design of the speaker. The speaker also supports wireless music playback from any device ' ||
+         'compatible with Wi-Fi or Bluetooth, and can be used as part of a Sonos multi-room system to play music ' ||
+         'in multiple rooms at the same time.', 199, 11, 10),
+        ('MacBook Air M1', '/images/MacBookAirM1.jpg', 2, 'MacBook Air M1 is an Apple laptop with an M1 processor ' ||
+         'that provides fast and efficient performance. It has a compact and lightweight body, making it convenient ' ||
+         'for transportation. The laptop has a high-resolution Retina display that provides bright and clear images. ' ||
+         'Additionally, it is equipped with a powerful battery that provides up to 15 hours of battery life on a ' ||
+         'single charge. The laptop also supports touch input technology and has a built-in fingerprint scanner ' ||
+         'for more secure access to the device.', 1299, 7, 1);
+INSERT INTO users(name, email, password, enabled)
+VALUES ('Test', 'test@gmail.com', 'test', true);
+INSERT INTO reviews (text, rating, user_id, product_id)
+VALUES ('Good product!', 4.8, 1, 1);
---
+++ src/main/resources/db/migration/V2023.05.08.1238__filling_with_test_data.sql
@@ -0,0 +1,43 @@
+INSERT INTO products(name, image, quantity, description, price, category_id, brand_id)
+VALUES ('MacBook Pro', '/images/MacBookPro.jpg', 10, 'The MacBook Pro is a line of Macintosh portable computers introduced in January ' ||
+        '2006 by Apple Inc. It is the higher-end model of the MacBook family, sitting above the consumer-focused ' ||
+        'MacBook Air, and is available in 13-inch and 16-inch screen sizes. A 14-inch version was added in October ' ||
+        '2021. The MacBook Pro is designed for professionals who need a powerful and portable computer for tasks ' ||
+        'such as video editing, graphic design, and software development.', 1299, 1, 1),
+       ('iPhone 13 Pro Max', '/images/iPhone13ProMax.jpg', 20, 'The iPhone 13 Pro Max is a smartphone designed and marketed by Apple Inc. ' ||
+        'It is the larger version of the iPhone 13 Pro, with a 6.7-inch Super Retina XDR display. The phone features ' ||
+        'a triple-lens rear camera system with 12-megapixel Ultra Wide, Wide, and Telephoto cameras, as well as the ' ||
+        'ability to capture ProRAW images. It is powered by the A15 Bionic chip and is available with up to 1TB of ' ||
+        'storage. Other features include 5G connectivity, Face ID, and water resistance.', 1099, 2, 1),
+       ('Samsung Galaxy Tab S7+', '/images/GalaxyTabS7.jpg', 5, 'The Samsung Galaxy Tab S7+ is a tablet computer designed and ' ||
+        'manufactured by Samsung Electronics. It features a 12.4-inch Super AMOLED display with a resolution of ' ||
+        '2800x1752 pixels, and is powered by the Qualcomm Snapdragon 865+ processor. The tablet comes with an S Pen ' ||
+        'stylus for note-taking and drawing, and supports 5G connectivity. It runs on the Android operating system ' ||
+        'and is available with up to 512GB of storage.', 849, 3, 2),
+       ('Sony X90J 65-Inch TV', '/images/SonyX90J.jpg', 8, 'The Sony X90J 65-Inch TV is a high-end television designed and ' ||
+        'manufactured by Sony Corporation. It features a 65-inch 4K HDR display with Triluminos Pro and X1 4K HDR ' ||
+        'Processor, which delivers vivid colors and detailed contrast. The TV also features a 120Hz refresh rate, ' ||
+        'and supports advanced gaming features such as HDMI 2.1 and VRR for smooth gaming performance. It runs on ' ||
+        'the Google TV operating system and is compatible with Amazon Alexa and Google Assistant.', 1299, 4, 3),
+       ('Canon EOS R6', '/images/CanonEOSR6.jpg', 4, 'The Canon EOS R6 is a full-frame mirrorless camera designed and manufactured ' ||
+        'by Canon Inc. It features a 20.1-megapixel CMOS sensor, and is powered by the DIGIC X image processor. ' ||
+        'The camera has a fast autofocus system with up to 1053 AF points, and can shoot up to 12 frames per second. ' ||
+        'It also supports 4K video recording and has in-body image stabilization. Other features include Wi-Fi and ' ||
+        'Bluetooth connectivity, as well as compatibility with Canon''s RF lens lineup.', 2499, 5, 7),
+       ('Sony Bravia XR A80J', '/images/SonyBraviaXRA80J.jpg', 20, 'The Sony Bravia XR A80J is a stunning 4K OLED TV with a beautiful ' ||
+        'design and advanced features. With its powerful X1 Ultimate processor and support for Dolby Vision and ' ||
+        'Atmos, this TV delivers an immersive viewing experience that''s perfect for movie and TV fans.', 2999, 4, 3),
+       ('HP Color LaserJet Enterprise MFP M477', '/images/HPColorLaserJet.jpg', 15, 'The HP Color LaserJet Enterprise MFP M477 is a ' ||
+        'powerful all-in-one printer that can print, scan, copy, and fax. With its fast printing speeds and ' ||
+        'high-quality output, this printer is perfect for businesses and offices that need to produce ' ||
+        'professional-quality documents.', 629, 10, 8),
+       ('Sonos Beam', '/images/SonosBeam.jpg', 25, 'The Sonos Beam is a compact soundbar that delivers powerful and immersive sound ' ||
+        'for movies, TV shows, and music. With its support for Amazon Alexa and Google Assistant, this soundbar is ' ||
+        'perfect for anyone who wants to control their entertainment with their voice.', 399, 11, 10),
+       ('Sony Alpha a7 III', '/images/SonyAlphaA7III.jpg', 7, 'Sony Alpha a7 III is a full-frame mirrorless camera that features a 24.2MP ' ||
+        'sensor and can shoot up to 10 frames per second. It has a 5-axis image stabilization system and supports ' ||
+        '4K video recording. It comes with built-in Wi-Fi and NFC for easy sharing and remote control.', 1999, 5, 3),
+       ('Samsung Galaxy Watch 4 Classic', '/images/GalaxyWatch4Classic.jpg', 20, 'The Samsung Galaxy Watch 4 Classic is a premium smartwatch ' ||
+        'with a stainless steel frame and a rotating bezel for easy navigation. It has a 1.4-inch Super AMOLED ' ||
+        'display and runs on Samsung''s One UI Watch 3. It also has built-in GPS, heart rate monitoring, and sleep ' ||
+        'tracking. It runs on Wear OS and has access to the Google Play Store and its apps.', 349, 8, 2);
