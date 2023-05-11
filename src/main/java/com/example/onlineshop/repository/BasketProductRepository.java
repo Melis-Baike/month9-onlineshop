00235132e8e5bbd77378d5ebddbd9ba2fb44395a
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-11 18:29:34 +0600

    Changed repository classes ( new queries )

--- src/main/java/com/example/onlineshop/repository/BasketProductRepository.java
+++ src/main/java/com/example/onlineshop/repository/BasketProductRepository.java
@@ -4,6 +4,7 @@ import com.example.onlineshop.entity.BasketProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BasketProductRepository extends JpaRepository<BasketProduct, Long> {
 }
--- src/main/java/com/example/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/example/onlineshop/repository/ProductRepository.java
@@ -5,7 +5,11 @@ import com.example.onlineshop.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -21,4 +25,9 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByNameAndPrice(String name, Double price, Pageable pageable);
     Page<Product> findAllByNameAndCategory(String name, Category category, Pageable pageable);
     Page<Product> findAllByPriceAndCategory(Double price, Category category, Pageable pageable);
+
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE Product d SET d.quantity = :quantity WHERE d.id = :id")
+    void setQuantityById(@Param("quantity") Long quantity, @Param("id") Long id);
 }
