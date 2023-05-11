e76b483a71aba85c5de20038a5a72a4ca86e3478
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-11 18:23:03 +0600

    Also added new features in .css and .js files. File .html moved to templates

--- src/main/resources/static/frontend/index.js
+++ src/main/resources/static/frontend/index.js
@@ -3,10 +3,14 @@ const registerForm = document.getElementById('register-form');
 const loginForm = document.getElementById('login-form');
 const splashArea = document.getElementById('splash');
 const navbar = document.getElementById('navbar');
-const userProfile = document.getElementById('userProfile');
 const searchingForm = document.getElementById('searching-form');
 const productArea = document.getElementById('productArea');
 const page = document.getElementById('page');
+const main = document.getElementById('navbar-brand');
+const logout = document.getElementById('logout');
+const basket = document.getElementById('basket');
+const productQuantityModal = document.getElementById('productQuantityModal');
+const saveBtn = document.getElementsByClassName('saveBtn')[0];
 
 $('.message a').click(function(){
     $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
@@ -17,9 +21,7 @@ async function registrationHandler(e){
     e.preventDefault();
     const form = e.target;
     const data = new FormData(form);
-    console.log(data);
     const json = parseJson(data);
-    console.log(json);
     axios.post('http://127.0.0.1:8080/users/registration', json, {
         headers : {
             'Content-Type':'application/json'
@@ -40,7 +42,8 @@ function loginHandler(e){
     saveUser(user);
     axios.post('http://127.0.0.1:8080/users/auth', user, updateOptions({}))
         .then(function (response){
-            hideLoginPage(user);
+            console.log(response);
+            hideLoginPage();
         }).catch(function (error) {
         console.log(error);
         localStorage.clear();
@@ -88,20 +91,20 @@ async function userCheckHandler(user) {
         }
         console.log('user exists')
         // const account = await axios.post('http://127.0.0.1:8080/users/auth', dto, updateOptions({}));
-        console.log();
-        hideLoginPage(dto);
+        hideLoginPage();
 
     } else {
         console.log('user doesn\'t exist');
     }
 }
 
-function hideLoginPage(user){
+function hideLoginPage(){
     authPage.className = '';
     splashArea.className = '';
     authPage.style.display = 'none';
     navbar.style.display = 'flex';
-    userProfile.innerText = user.email;
+    page.style.display = 'block';
+    // userProfile.innerText = user.email;
 }
 
 searchingForm.addEventListener('submit', searchFormHandler)
@@ -109,18 +112,15 @@ function searchFormHandler(e){
     e.preventDefault();
     const form = e.target;
     const data = new FormData(form);
-    console.log(data.get('name') + data.get('price') + data.get('category'));
     axios.get('http://127.0.0.1:8080/products', updateOptions({ headers : {
             'Name': data.get('name'),
             'Price': data.get('price'),
             'Category': data.get('category')
         }
     })).then(function (response){
-        console.log(response.data.content);
+        productArea.innerHTML = '';
         for (let i = 0; i < response.data.content.length; i++) {
-            console.log(response.data.content[i]);
             let layout = createProductElement(response.data.content[i]);
-            console.log(layout)
             let nDoc = parse(layout);
             productArea.append(nDoc.body.getElementsByClassName('productElement')[0]);
             let information = document.getElementsByClassName('productInfo');
@@ -128,7 +128,11 @@ function searchFormHandler(e){
             info.addEventListener('click', infoHandler);
             let reviews = document.getElementsByClassName('review');
             let review = reviews[reviews.length - 1];
-            review.addEventListener('click', reviewHandler)
+            review.addEventListener('click', reviewHandler);
+            let putBtns = document.getElementsByClassName('putBtn');
+            let putBtn = putBtns[putBtns.length - 1];
+            putBtn.addEventListener('click', formatProductModalWindow);
+            saveBtn.addEventListener('click', putInBasketBtnHandler);
         }
     }).catch(function (error){
         console.log(error);
@@ -141,20 +145,26 @@ function parse(element){
 }
 
 function createProductElement(product){
-    return '<div class="card productElement" style="width: 18rem;" id="product' + product.id + '">\n' +
-        '  <img class="card-img-top" src="..' + product.image + '" alt="Card image cap">\n' +
-        '  <div class="card-body">\n' +
+    return '<div class="card productElement" style="width: 21rem;" id="product' + product.id + '">\n' +
+        '  <img class="card-img-top productCardImage" src="/static/images/' + product.image + '" alt="Card image cap">\n' +
+        '  <div class="card-body productCardBody">\n' +
         '    <h5 class="card-title">' + product.name + '</h5>\n' +
         '    <p class="card-text">' + product.description + '</p>\n' +
-        '    <a href="#" class="btn btn-primary review">Reviews</a>\n' +
-        '    <a href="#" class="btn btn-primary productInfo">More info</a>\n' +
+        '    <div class="cardBtns">' +
+        '       <a href="#" class="btn btn-primary review cardBtn">Reviews</a>\n' +
+        '       <a href="#" class="btn btn-primary productInfo cardBtn">More info</a>\n' +
+        '       <button type="button" class="btn btn-primary putBtn cardBtn" data-bs-toggle="modal" data-bs-target="#productQuantityModal" ' +
+        '           id="modalBtn">\n' +
+        '           Put in the basket\n' +
+        '       </button>\n' +
+        '    </div>' +
         '  </div>\n' +
         '</div>'
 }
 
 function infoHandler(e){
     e.preventDefault();
-    const form = e.target.parentNode;
+    const form = e.target.parentNode.parentNode;
     const productName = form.getElementsByClassName('card-title')[0].innerText;
     axios.get('http://127.0.0.1:8080/products/' + productName, updateOptions({}))
         .then(function (response){
@@ -166,14 +176,12 @@ function infoHandler(e){
         }).catch(function (error){
         console.log(error);
     });
-    console.log(form);
-    console.log('aaa');
 }
 
 function createProductInfoElement(product){
-    return '<div class="card d-flex justify-content-center">\n' +
+    return '<div class="card d-flex justify-content-center more-info">\n' +
         '    <div class="card-header">\n' +
-        '<img src="..' + product.image + '" class="card-img-top align-self-center productInfoImage" ' +
+        '<img src="/static/images/' + product.image + '" class="card-img-top align-self-center productInfoImage" ' +
         ' alt="productImage">' +
         '    </div>\n' +
         '    <div class="card-body">\n' +
@@ -192,36 +200,28 @@ function createProductInfoElement(product){
 
 function reviewHandler(e){
     e.preventDefault();
-    const form = e.target.parentNode;
+    const form = e.target.parentNode.parentNode;
     const card = form.parentNode;
-    console.log(card)
     const productName = form.getElementsByClassName('card-title')[0].innerText;
     axios.get('http://127.0.0.1:8080/products/' + productName + '/reviews', updateOptions({}))
         .then(function (response){
-            console.log(response.data.content);
-            console.log(card.firstElementChild.src);
             page.style.display = 'none';
             const cardLayout = createProductReviewsCardElement(card);
             const cardDoc = parse(cardLayout);
             document.body.append(cardDoc.getElementsByClassName('card')[0]);
-            console.log(document.body.lastElementChild.getElementsByClassName('reviewArea')[0]);
-            console.log(response.data.content.length)
             for (let i = 0; i < response.data.content.length; i++) {
-                console.log(response.data.content[i]);
                 const reviewLayout = createReviewElement(response.data.content[i]);
-                console.log(reviewLayout);
                 const reviewDoc = parse(reviewLayout);
                 document.body.lastElementChild.getElementsByClassName('reviewArea')[0]
                     .append(reviewDoc.body.getElementsByClassName('reviewElement')[0]);
             }
-            console.log('response above');
         }).catch(function (error){
         console.log(error);
     });
 }
 
 function createProductReviewsCardElement(card){
-    return '<div class="card d-flex justify-content-center">\n' +
+    return '<div class="card d-flex justify-content-center more-info">\n' +
         '    <div class="card-header">\n' +
         '<img src="' + card.firstElementChild.src + '" class="card-img-top align-self-center productInfoImage" ' +
         ' alt="productImage">' +
@@ -230,9 +230,9 @@ function createProductReviewsCardElement(card){
         '        <h5 class="card-title">' + card.children[1].getElementsByClassName('card-title')[0].innerText + '</h5>\n' +
         '        <div class="reviewArea"></div>' +
         '    </div>\n' +
-        '    <div class="card-footer">\n' +
-        '        Футер карточки\n' +
-        '    </div>\n' +
+        // '    <div class="card-footer">\n' +
+        // '        Футер карточки\n' +
+        // '    </div>\n' +
         '</div>'
 }
 
@@ -243,3 +243,87 @@ function createReviewElement(review){
         '</div>'
 }
 
+main.addEventListener('click', mainHandler);
+function mainHandler(e){
+    e.preventDefault();
+    if(page.style.display === 'none') {
+        console.log('test info')
+        document.body.getElementsByClassName('more-info')[0].remove();
+        page.style.display = 'block';
+    }
+}
+
+logout.addEventListener('click', logoutHandler)
+function logoutHandler(e){
+    e.preventDefault();
+    localStorage.clear();
+    showSplashScreen();
+}
+
+function showSplashScreen(){
+    authPage.className = 'auth-page splash-header';
+    splashArea.className = 'splash';
+    authPage.style.display = 'flex';
+    page.style.display = 'none';
+    navbar.style.display = 'none';
+}
+
+
+function putInBasketBtnHandler(e){
+    e.preventDefault();
+    const submitForm = e.target.parentElement;
+    const form = e.target.parentNode.parentElement.parentElement;
+    const data = new FormData(submitForm);
+    const order = {
+        name: form.getElementsByClassName('card-title')[0].innerText,
+        quantity: data.get('quantity')
+    }
+    axios.post('http://127.0.0.1:8080/basket/ordering', order, updateOptions({}))
+        .then(function (response){
+            console.log(response);
+            const layout = createProductForBasket(form, data.get('quantity'));
+            const nDoc = parse(layout);
+            basket.append(nDoc.body.getElementsByClassName('card')[0]);
+            const quantity = form.getElementsByClassName('card-text')[0].innerText.substring(10) - data.get('quantity');
+            form.getElementsByClassName('card-text')[0].innerText = 'Quantity: ' + quantity;
+            let removeButtons = document.body.getElementsByClassName('remove');
+            let lastRemoveButton = removeButtons[removeButtons.length - 1];
+            lastRemoveButton.addEventListener('click', removeProductHandler);
+        }).catch(function (error){
+        console.log(error);
+    })
+}
+
+async function formatProductModalWindow(e){
+    e.preventDefault();
+    const form = e.target.parentElement.parentElement.parentElement;
+    const imageSrc = form.getElementsByClassName('card-img-top')[0].src;
+    const title = form.getElementsByClassName('card-title')[0].innerText;
+    const product = await axios.get('http://127.0.0.1:8080/products/' + title, updateOptions({}));
+    productQuantityModal.getElementsByClassName('card-img-top')[0].src = imageSrc;
+    productQuantityModal.getElementsByClassName('card-title')[0].innerText = title;
+    productQuantityModal.getElementsByClassName('card-text')[0].innerText = 'Quantity: ' + product.data.quantity;
+}
+
+while (basket.firstChild){
+    basket.removeChild(basket.firstChild);
+}
+
+function createProductForBasket(card, quantity){
+    const imageSrc = card.getElementsByClassName('card-img-top')[0].src;
+    const title = card.getElementsByClassName('card-title')[0].innerText;
+    return '<div class="card basketCard" style="width: 18rem;">\n' +
+        '  <img class="card-img-top basketCardImage" src="' + imageSrc + '" alt="Card image cap">\n' +
+        '  <div class="card-body">\n' +
+        '    <h5 class="card-title">' + title + '</h5>\n' +
+        '    <p class="card-text">Quantity: ' + quantity + '</p>\n' +
+        '    <a href="#" class="btn btn-primary remove" id="productRemoveFromBasket">Remove</a>\n' +
+        '  </div>\n' +
+        '</div>'
+}
+
+function removeProductHandler(e){
+    e.preventDefault();
+    const form = e.target.parentElement.parentElement;
+    form.remove();
+}
