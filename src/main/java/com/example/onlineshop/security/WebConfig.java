9bbf7d5863cfc142f2c67a02388d239744e9727b
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-15 13:31:58 +0600

    Added new method for CORS

--- src/main/java/com/example/onlineshop/security/WebConfig.java
+++ src/main/java/com/example/onlineshop/security/WebConfig.java
@@ -2,7 +2,10 @@ package com.example.onlineshop.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -16,5 +19,14 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/resources/**")
                 .addResourceLocations("classpath:/resources/")
                 .setCacheControl(CacheControl.noCache());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization")
+                .allowCredentials(true);
     }
 }
