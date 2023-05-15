0b77755f4ad9e4dcb8cc39fbefc7111f4e479953
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-15 13:27:19 +0600

    Added new features in repositories. Removed unnecessary code

--- src/main/java/com/example/onlineshop/repository/BasketProductRepository.java
+++ src/main/java/com/example/onlineshop/repository/BasketProductRepository.java
@@ -2,9 +2,17 @@ package com.example.onlineshop.repository;
 
 import com.example.onlineshop.entity.BasketProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Repository
 public interface BasketProductRepository extends JpaRepository<BasketProduct, Long> {
+    @Modifying
+    @Transactional
+    @Query(value = "UPDATE BasketProduct d SET d.productQuantity = :quantity WHERE d.id = :id")
+    void setProductQuantityById(@Param("quantity") Long quantity, @Param("id") Long id);
 }
--- src/main/java/com/example/onlineshop/repository/ProductRepository.java
+++ src/main/java/com/example/onlineshop/repository/ProductRepository.java
@@ -1,33 +1,77 @@
 package com.example.onlineshop.repository;
 
+import com.example.onlineshop.entity.Brand;
 import com.example.onlineshop.entity.Category;
 import com.example.onlineshop.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
-
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
     Optional<Product> findByName(String name);
-    Page<Product> findAllByName(String name, Pageable pageable);
-    Page<Product> findAllByPrice(Double price, Pageable pageable);
-    Page<Product> findAllByCategory(Category category, Pageable pageable);
-
-    Page<Product> findAllByNameAndPriceAndCategory(String name, Double price, Category category, Pageable pageable);
-
-    Page<Product> findAllByNameAndPrice(String name, Double price, Pageable pageable);
-    Page<Product> findAllByNameAndCategory(String name, Category category, Pageable pageable);
-    Page<Product> findAllByPriceAndCategory(Double price, Category category, Pageable pageable);
 
     @Modifying
     @Transactional
     @Query(value = "UPDATE Product d SET d.quantity = :quantity WHERE d.id = :id")
     void setQuantityById(@Param("quantity") Long quantity, @Param("id") Long id);
+
+
+    default Page<Product> searchProducts(Optional<String> name, Optional<String> minPrice, Optional<String> maxPrice,
+                                         Optional<Category> category, Optional<String> quantity, Optional<Brand> brand,
+                                         Optional<String> description, Pageable pageable) {
+        if (maxPrice.isPresent() && minPrice.isPresent() && !maxPrice.get().isBlank() && !minPrice.get().isBlank()
+                && Double.parseDouble(maxPrice.get()) < Double.parseDouble(minPrice.get())) {
+            return Page.empty();
+        }
+
+        return findAll((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+
+            name.ifPresent(s -> {
+                if(!s.isBlank()) {
+                    predicates.add(criteriaBuilder.equal(root.get("name"), s));
+                }
+            });
+
+            quantity.ifPresent(qnt -> {
+                if(!qnt.isBlank()){
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("quantity"), Integer.parseInt(qnt)));
+                }
+            });
+
+            description.ifPresent(descr -> {
+                if(!descr.isBlank()){
+                    predicates.add(criteriaBuilder.like(root.get("description"), "%" + descr + "%"));
+                }
+            });
+
+            minPrice.ifPresent(price -> {
+                if(!price.isBlank()) {
+                    predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("price"), Double.parseDouble(price)));
+                }
+            });
+            maxPrice.ifPresent(price -> {
+                if(!price.isBlank()) {
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("price"), Double.parseDouble(price)));
+                }
+            });
+
+            category.ifPresent(cat -> predicates.add(criteriaBuilder.equal(root.get("category"), cat)));
+
+            brand.ifPresent(br -> predicates.add(criteriaBuilder.equal(root.get("brand"), br)));
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        }, pageable);
+    }
 }
