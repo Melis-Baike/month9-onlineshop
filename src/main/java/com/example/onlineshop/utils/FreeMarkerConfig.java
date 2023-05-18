fa483e4b149aaa7c80c95242548687bcc0024b87
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-18 15:36:47 +0600

    Added FreeMarker configuration class

---
+++ src/main/java/com/example/onlineshop/utils/FreeMarkerConfig.java
@@ -0,0 +1,17 @@
+package com.example.onlineshop.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+@Configuration
+public class FreeMarkerConfig {
+
+    @Bean
+    public FreeMarkerConfigurer freeMarkerConfigurer() {
+        FreeMarkerConfigurer configurer = new FreeMarkerConfigurer();
+        configurer.setTemplateLoaderPath("classpath:/templates/");
+        configurer.setDefaultEncoding("UTF-8");
+        return configurer;
+    }
+}
