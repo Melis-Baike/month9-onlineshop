172e2aba0bd7a23b0d401df03b5cd5976dc999d6
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-18 15:37:15 +0600

    Changed controller classes

--- src/main/java/com/example/onlineshop/controller/RootHandler.java
+++ src/main/java/com/example/onlineshop/controller/RootHandler.java
@@ -1,22 +1,15 @@
 package com.example.onlineshop.controller;
 
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 @RequestMapping("/")
 public class RootHandler {
     @GetMapping()
-    public ResponseEntity<Resource> home() {
-        Resource resource = new ClassPathResource("/templates/index.html");
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_HTML_VALUE)
-                .body(resource);
+    public String home() {
+        return "index";
     }
 }
--- src/main/java/com/example/onlineshop/controller/UserController.java
+++ src/main/java/com/example/onlineshop/controller/UserController.java
@@ -1,32 +1,78 @@
 package com.example.onlineshop.controller;
 
-import com.example.onlineshop.DTO.UserDTO;
 import com.example.onlineshop.DTO.UserRegistrationDTO;
 import com.example.onlineshop.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-@RestController
+@Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("/registration")
-    public ResponseEntity<String> register(@Valid @RequestBody UserRegistrationDTO userRegistrationDTO){
-        return new ResponseEntity<>(userService.register(userRegistrationDTO), HttpStatus.OK);
+    @PostMapping("/register/post")
+    public String registerPage(@ModelAttribute @Valid UserRegistrationDTO userRegistrationDTO, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            return "unsuccessfulRegEntry";
+        }
+        String response = userService.register(userRegistrationDTO);
+        if(response.equals("Enter another email")){
+            return "unsuccessfulRegEntry";
+        }
+        return "successfulRegEntry";
     }
 
-    @PostMapping("/auth")
-    public UserDTO login(Authentication authentication){
-        return userService.login(authentication);
+    @GetMapping("/register")
+    public String pageRegisterCustomer() {
+        return "register";
+    }
+
+
+    @GetMapping("/auth")
+    public String auth(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getPrincipal() != "anonymousUser") {
+            userService.login(authentication);
+            return "redirect:/";
+        } else {
+            return "login";
+        }
+    }
+
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @PostMapping("/unsuccessfulLoginEntry")
+    public String showPostUnsuccessfulLogin() {
+        return "unsuccessfulLoginEntry";
+    }
+
+    @GetMapping("/unsuccessfulLoginEntry")
+    public String showGetUnsuccessfulLogin() {
+        return "unsuccessfulLoginEntry";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login?logout";
     }
 }
