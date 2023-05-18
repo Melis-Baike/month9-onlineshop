759b130f7927db8135304bd4563077e0d0b00779
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-18 15:37:29 +0600

    Changed service class

--- src/main/java/com/example/onlineshop/service/UserService.java
+++ src/main/java/com/example/onlineshop/service/UserService.java
@@ -1,12 +1,13 @@
 package com.example.onlineshop.service;
 
-import com.example.onlineshop.security.SecurityConfiguration;
 import com.example.onlineshop.DTO.UserDTO;
+import com.example.onlineshop.DTO.UserLoginDTO;
 import com.example.onlineshop.DTO.UserRegistrationDTO;
-import com.example.onlineshop.entity.Role;
 import com.example.onlineshop.entity.User;
-import com.example.onlineshop.entity.UserRole;
-import com.example.onlineshop.repository.*;
+import com.example.onlineshop.repository.AuthorityRepository;
+import com.example.onlineshop.repository.UserRepository;
+import com.example.onlineshop.security.SecurityConfiguration;
+import com.example.onlineshop.utils.PasswordUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -16,9 +17,8 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final RoleRepository roleRepository;
+    private final AuthorityRepository authorityRepository;
     private final UserRepository userRepository;
-    private final UserRoleRepository userRoleRepository;
 
 
     public String register(UserRegistrationDTO userRegistrationDTO){
@@ -29,19 +29,17 @@ public class UserService {
                     .email(userRegistrationDTO.getEmail())
                     .password(SecurityConfiguration.passwordEncoder().encode(userRegistrationDTO.getPassword()))
                     .enabled(true)
+                    .authority(authorityRepository.findById(2L).get())
                     .build();
-            Optional<Role> role = roleRepository.findById(2L);
-            if (role.isPresent()) {
-                UserRole userRole = UserRole.builder()
-                        .user(user)
-                        .role(role.get())
-                        .build();
-                userRepository.save(user);
-                userRoleRepository.save(userRole);
-                return "You have successfully registered";
-            }
+            userRepository.save(user);
+            return "You have successfully registered";
         }
-        return "Something went wrong";
+        return "Enter another email";
+    }
+
+    public boolean login(UserLoginDTO userLoginDTO){
+        Optional<User> user = userRepository.findByEmail(userLoginDTO.getEmail());
+        return user.filter(value -> PasswordUtils.checkPassword(userLoginDTO.getPassword(), value.getPassword())).isPresent();
     }
 
     public UserDTO login(Authentication authentication){
