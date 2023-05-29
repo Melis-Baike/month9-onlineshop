d7a9638908ccad4573fc9e996458df7ad88ebac7
Erkin Imakeev <erkinimakeev7@gmail.com>
2023-05-29 14:21:58 +0600

    Deleted unnecessary class

--- src/main/java/com/example/onlineshop/utils/PasswordUtils.java
+++
@@ -1,9 +0,0 @@
-package com.example.onlineshop.utils;
-
-import org.springframework.security.crypto.bcrypt.BCrypt;
-
-public class PasswordUtils {
-    public static boolean checkPassword(String plainPassword, String hashedPassword) {
-        return BCrypt.checkpw(plainPassword, hashedPassword);
-    }
-}
